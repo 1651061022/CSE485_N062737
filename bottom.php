4f8ec1ac0eb111cbbe24ad6e98f78aa445749d2b
Dau Duc Manh <manhdd62@wru.vn>
2018-12-06 14:15:01 +0700

    push project

---
+++ bottom.php
@@ -0,0 +1,176 @@
+
+<footer>
+	<div class="container">
+		<div class="row">
+			<div class="footer-c1 footer">
+				<div class="col-md-3">
+					<div class="row">
+						<h1>CÁCH THỨC THANH TOÁN:</h1>
+					</div>
+				</div>
+			</div>	
+			<div class="footer-c2 footer">
+				<div class="col-md-3">
+					<div class="row">
+						<h1>CHÍNH SÁCH CHUNG:</h1>
+						<p>Trả góp, lãi suất</p>
+						<p>Bảo trì và bảo hành</p>
+						<p>Tuyển dụng</p>
+						<p>Hỏi đáp mua hàng Online</p>
+					</div>
+				</div>	
+				<div class="footer-c3 footer">
+					<div class="col-md-3">
+						<div class="row">
+							<h1>HỖ TRỢ:</h1>
+							<p>Giới thiệu công ty</p>
+							<p>Bảo mật thông tin</p>
+							<p>Hướng dẫn mua hàng</p>
+							<p>Hướng dẫn thanh Toán</p>
+						</div>
+					</div>
+				</div>	
+				<div class="footer-c4 footer">
+					<div class="col-md-3">
+						<div class="row">
+							<h1>THÔNG TIN LIÊN HỆ:</h1>
+							<p>Gọi mua hàng: 1900.6903 (8H30 - 20H00)</p>
+							<p>Gọi bảo hành: 1900.6903 (8H30 - 20H00)</p>
+							<p>Hotline: 091.619.6629</p>
+							<p>Hướng dẫn online:</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
+<div class="modal fade" id="myModal2" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<h4 class="modal-title" id="myModalLabel">Popup login</h4>
+			</div>
+			<div class="modal-body">
+				<div class="form">
+					<form >
+						<h1>Registration Form</h1>
+						<div class='input'>
+							<p>First Name: </p>
+							<input type="text" name="firstname">
+						</div>
+						<div class="input" >
+							<p>Last Name: </p>
+							<input type="text" name="lastname"><br/>
+						</div>
+						<div class="input" >
+							<p>Email: </p>
+							<input type="text" name="email"><br/>
+						</div>
+						<div class="elem_select">
+							<p>Ngay Sinh: </p>	
+							<div class="form_birth Day">
+								<select name="NgaySinh">
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+									<option value="13">13</option>
+									<option value="14">14</option>
+									<option value="15">15</option>
+									<option value="16">16</option>
+									<option value="17">17</option>
+									<option value="18">18</option>
+									<option value="19">19</option>
+									<option value="20">20</option>
+									<option value="21">21</option>
+									<option value="22">22</option>
+									<option value="23">23</option>
+									<option value="24">24</option>
+									<option value="25">25</option>
+									<option value="26">26</option>
+									<option value="27">27</option>
+									<option value="28">28</option>
+									<option value="29">29</option>
+									<option value="30">30</option>
+									<option value="31">31</option>
+								</select>
+							</div>
+							<div class="form_birth Month">
+								<select name="ThangSinh">
+									<option value="1">1</option>
+									<option value="2">2</option>
+									<option value="3">3</option>
+									<option value="4">4</option>
+									<option value="5">5</option>
+									<option value="6">6</option>
+									<option value="7">7</option>
+									<option value="8">8</option>
+									<option value="9">9</option>
+									<option value="10">10</option>
+									<option value="11">11</option>
+									<option value="12">12</option>
+								</select>
+							</div>
+							<div class="form_birth Years">
+								<select name="NamSinh">
+									<option value="1998">1998</option>
+									<option value="1">1</option>
+									<option value="1">1</option>
+									<option value="1">1</option>
+									<option value="1">1</option>
+									<option value="1">1</option>
+								</select>
+							</div>
+						</div>
+						<div class="input">
+							<p>Gioi Tinh</p>
+							<div class="a_radio">
+								<input type="radio" name="gender" value="male" checked> Male<br>
+								<input type="radio" name="gender" value="female"> Female<br>
+								<input type="radio" name="gender" value="other"> Other 
+							</div>
+						</div>
+						<div class="input">
+							<p>UserName: </p>
+							<input type="text" name="username"><br/>
+						</div>
+						<div class="input">
+							<p>PassWord: </p>
+							<input type="password" name="pass"><br/>
+						</div>
+
+						<div class="Btn_Register button ">
+							<a href="index2.html">
+								<button>Dang Ki</button>
+							</a>
+						</div>
+					</form>
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+
+				<a href="#">
+					<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal2">
+					Dang Ki</button>
+				</a>
+				<button type="button" class="btn btn-primary">Dang Nhap</button>
+			</div>
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="js/myscript.js"></script>
+</body>
+</html>
---
+++ top.php
@@ -0,0 +1,130 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>H&M-Travel</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap-theme.min.css">
+	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css">
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+	<div class="container-fluid  bggd">
+		<div class="row">
+			<div class="container">
+				<div class="row">
+					<header>
+						<div class="col-md-2">
+							<div class="row">
+								<div class="logo">		
+									<a href="index.html">
+										<img src="image/logo.png">
+									</a>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-3 col-md-offset-7">
+							<div class="row">
+								<div class="loginbtn">
+									<div class="button">
+										<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+											Đăng Nhập / login
+										</button>
+										<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+											<div class="modal-dialog" role="document">
+												<div class="modal-content">
+													<div class="modal-header">
+														<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+															<span aria-hidden="true">&times;</span>
+														</button>
+														<h4 class="modal-title" id="myModalLabel">Popup login</h4>
+													</div>
+													<div class="modal-body">
+														<div class="main">
+															<div class="form2">
+																<div>
+																	<h1>Login</h1>
+																	<div class="input2">
+																		<p>Nhap Username: </p>
+																		<input type="text" name="userid">
+																		<p>Nhap Password: </p>
+																		<input type="password" name="pass"><br/>
+																	</div>
+																	
+																</div>
+															</div>
+														</div>
+													</div>
+													<div class="modal-footer">
+														<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+														
+														<a href="#">
+															<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#myModal2">
+															Dang Ki</button>
+														</a>
+														<button type="button" class="btn btn-primary">Dang Nhap</button>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="col-md-12">
+							<div class="row">
+								<div class="topnav">
+									<a href="#home">Trang Chủ</a>
+									<a href="#news">Du Lịch</a>
+									<a href="#contact">Khách Sạn</a>
+									<a href="#about">Ăn Uống</a>
+									<a href="#about">Tin Tức</a>
+									<a href="#about">CSKH</a>
+									<a href="#about">Liên Hệ</a>
+								</div>
+							</div>
+						</div>
+					</header>
+					<div class="col-md-11">
+						<div class="row">
+							<div class="slideshow-container">
+								<div class="mySlides fade">
+									<div class="numbertext"></div>
+									<img src="image/slideshow/1.jpg" style="width:109%">
+									<div class="text">1</div>
+								</div>
+								<div class="mySlides fade">
+									<div class="numbertext"></div>
+									<img src="image/slideshow/2.jpg" style="width:109%">
+									<div class="text">2</div>
+								</div>
+								<div class="mySlides fade">
+									<div class="numbertext"></div>
+									<img src="image/slideshow/3.jpg" style="width:109%">
+									<div class="text">3</div>
+								</div>
+								<div class="mySlides fade">
+									<div class="numbertext"></div>
+									<img src="image/slideshow/4.jpg" style="width:109%">
+									<div class="text">4</div>
+								</div>
+								<div class="mySlides fade">
+									<div class="numbertext"></div>
+									<img src="image/slideshow/5.jpg" style="width:109%">
+									<div class="text">5</div>
+								</div>
+								<div style="text-align:center;margin: 12px">
+									<span class="dot"></span> 
+									<span class="dot"></span>
+									<span class="dot"></span> 
+									<span class="dot"></span> 
+									<span class="dot"></span>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
