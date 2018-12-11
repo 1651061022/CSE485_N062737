78cdf41fa2369b7eb4d0057df591fcd5df441b23
Dau Duc Manh <manhdd62@wru.vn>
2018-12-11 17:48:40 +0700

    update n+1

--- 2_header.php
+++ 2_header.php
@@ -10,31 +10,24 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-8 col-md-6">
+			<div class="col-xs-12 col-md-6">
 				<div class="ro">	
 					<div class="search-container">
-						<form action="/action_page.php">
-							<input type="text" placeholder="Search.." name="search">
-							<button type="button" class="btn btn-default btn-lg">
-								<span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-							</button>
+						<form action="https://www.google.com/search" class="searchform" method="get" name="searchform" target="_blank">
+							<input name="sitesearch" type="hidden" value="">
+							<i class="fab fa-google"></i>
+							<input autocomplete="on" class="form-control search" name="q" placeholder="GOOGLE-Search..." required="required"  type="text">
+							<button class="button" type="submit"><i class="fas fa-search"></i></button>
 						</form>
 					</div>
 				</div>
 			</div>
-			<div class="col-xs-4 col-md-3">
+			<div class="col-md-3">
 				<div class="form_logn">
-					<div class="col-lg-6">
-					<!-- <div class="loginbtn">
-						<div class="button">
-							<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
-								Đăng Nhập / login </button>
-						</div>
-					</div> -->
 					<div class="style2">
 						<button onclick="document.getElementById('id01').style.display='block'" style="width:auto;">Register</button>
 						<div id="id01" class="modal">
-							<form class="modal-content animate" action="" method="post">
+							<form class="modal-content animate" action="" method="POST">
 								<div class="container conta" >
 									<div class="row">
 										<div class="cot1 col-md-4">           
@@ -58,10 +51,11 @@
 												<label for="username"><b>BirthDay: </b></label>
 												<div class="form-group">
 													<div class='input-group date' id='datetimepicker'>
-														<input type='text' class="form-control" placeholder="BirthDay" name="birthday" data-format="YYYY/MM/DD"/>
+														<input type="text" class="form-control" placeholder="BirthDay" name="birthday" data-format="YYYY/MM/DD" style="margin-bottom: 0px;">
 														<span class="input-group-addon">
 															<span class="glyphicon glyphicon-time"></span>
 														</span>
+
 													</div>
 												</div>
 											</div>
@@ -98,37 +92,32 @@
 								</div>
 							</form>
 						</div>
-					</div>
-				</div>
-				<div class="col-md-6">
-					<div class="style2">
 						<button onclick="document.getElementById('id02').style.display='block'" style="width:auto;">Login</button>
 
 						<div id="id02" class="modal">
-							<form class="modal-content animate" action="/action_page.php" method="GET">
+							<form class="modal-content animate" action="/action_page.php" method="POST">
 								<div class="container conta">
-
 									<div class="col-md-9">
 										<label for="uname"><b>Username</b></label>
 										<input type="text" placeholder="Enter Username" name="uname" required>
-
 										<label for="psw"><b>Password</b></label>
 										<input type="password" placeholder="Enter Password" name="psw" required>
 									</div>
 									<div class="col-md-3">
 										<div class="imgcontainer">
-											<span onclick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">&times;</span>
+											<span onclick="document.getElementById('id02').style.display='none'" class="close" title="Close Modal">&times;</span>
 											<img src="image/avatar.png" alt="Avatar" class="avatar">
 										</div>
 									</div>
-
-									<button type="submit">Login</button>
-									<label>
-										<input type="checkbox" checked="checked" name="remember"> Remember me
-									</label>
+									<div class="col-md-12">
+										<button type="submit">Login</button>
+										<label>
+											<input type="checkbox" checked="checked" name="remember"> Remember me
+										</label>
+									</div>
 								</div>
 								<div class="container conta" style="background-color:#f1f1f1">
-									<button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+									<button type="button" onclick="document.getElementById('id02').style.display='none'" class="cancelbtn">Cancel</button>
 									<span class="psw">Forgot <a href="#">password?</a></span>
 								</div>
 							</form>
@@ -136,13 +125,22 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="col-xs-12 col-md-12 ">
-			<div class="row">
-				<div class="dropdown-navbar">
-					<button onclick="myFunction()"  class="dropbtn">Menu
-					</button>
-					<div id="myDropdown" class="dropdown-content">
+			<div class="col-xs-12 col-md-12 ">
+				<div class="row">
+					<div class="dropdown-navbar">
+						<button onclick="myFunction()"  class="dropbtn">Menu
+						</button>
+						<div id="myDropdown" class="dropdown-content">
+							<a href="3_main.php">Trang Chủ</a>
+							<a href="next1.php">Du Lịch</a>
+							<a href="next1.php">Khách Sạn</a>
+							<a href="next1.php">Ăn Uống</a>
+							<a href="index.php">Tin Tức</a>
+							<a href="index.php">CSKH</a>
+							<a href="index.php">Liên Hệ</a>
+						</div>
+					</div>
+					<div class="topnav">
 						<a href="3_main.php">Trang Chủ</a>
 						<a href="next1.php">Du Lịch</a>
 						<a href="next1.php">Khách Sạn</a>
@@ -152,57 +150,47 @@
 						<a href="index.php">Liên Hệ</a>
 					</div>
 				</div>
-				<div class="topnav">
-					<a href="3_main.php">Trang Chủ</a>
-					<a href="next1.php">Du Lịch</a>
-					<a href="next1.php">Khách Sạn</a>
-					<a href="next1.php">Ăn Uống</a>
-					<a href="index.php">Tin Tức</a>
-					<a href="index.php">CSKH</a>
-					<a href="index.php">Liên Hệ</a>
-				</div>
 			</div>
-		</div>
-	</header>
-	<div class="col-md-12">
-		<div class="row">
-			<div class="slideshow-container">
-				<div class="mySlides fade">
-					<div class="numbertext"></div>
-					<img src="image/slideshow/1.jpg" >
-					<div class="text">1</div>
-				</div>
-				<div class="mySlides fade">
-					<div class="numbertext"></div>
-					<img src="image/slideshow/2.jpg" >
-					<div class="text">2</div>
-				</div>
-				<div class="mySlides fade">
-					<div class="numbertext"></div>
-					<img src="image/slideshow/3.jpg" >
-					<div class="text">3</div>
-				</div>
-				<div class="mySlides fade">
-					<div class="numbertext"></div>
-					<img src="image/slideshow/4.jpg" >
-					<div class="text">4</div>
-				</div>
-				<div class="mySlides fade">
-					<div class="numbertext"></div>
-					<img src="image/slideshow/5.jpg" >
-					<div class="text">5</div>
-				</div>
-				<div style="text-align:center;margin: 12px">
-					<span class="dot"></span> 
-					<span class="dot"></span>
-					<span class="dot"></span> 
-					<span class="dot"></span> 
-					<span class="dot"></span>
+		</header>
+		<div class="col-md-12">
+			<div class="row">
+				<div class="slideshow-container">
+					<div class="mySlides fade">
+						<div class="numbertext"></div>
+						<img src="image/slideshow/1.jpg" >
+						<div class="text">1</div>
+					</div>
+					<div class="mySlides fade">
+						<div class="numbertext"></div>
+						<img src="image/slideshow/2.jpg" >
+						<div class="text">2</div>
+					</div>
+					<div class="mySlides fade">
+						<div class="numbertext"></div>
+						<img src="image/slideshow/3.jpg" >
+						<div class="text">3</div>
+					</div>
+					<div class="mySlides fade">
+						<div class="numbertext"></div>
+						<img src="image/slideshow/4.jpg" >
+						<div class="text">4</div>
+					</div>
+					<div class="mySlides fade">
+						<div class="numbertext"></div>
+						<img src="image/slideshow/5.jpg" >
+						<div class="text">5</div>
+					</div>
+					<div style="text-align:center;margin: 12px">
+						<span class="dot"></span> 
+						<span class="dot"></span>
+						<span class="dot"></span> 
+						<span class="dot"></span> 
+						<span class="dot"></span>
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-</div>
 </div>
 <div class="Left-Content">
 	<div class="Sidenav-left">
--- 3_main.php
+++ 3_main.php
@@ -5,14 +5,6 @@
 	include '1_head.php';
 	?>
 </head>
-<style type="text/css">
-.container.id01{
-  width: 100% !important;
-}
-.date.id01 input{
-  margin-top: 0px !important;
-}	
-</style>
 <body>
 	<div class="container-fluid  bggd">
 		<div class="row">
@@ -30,10 +22,10 @@
 							</div>
 							<div class="col-md-6">
 								<div class="bantinchinh">
-									<a href="next1.php">
+									<a href="http://localhost/btl/next1.php">
 										<img src="image/halong2.jpeg">
 									</a>
-									<a href="next1.php">
+									<a href="http://localhost/btl/next1.php">
 										<div class="title-news">DU LỊCH ĐÀ NẴNG - BÀ NÀ HILL - SƠN TRÀ - HỘI AN :</div>
 									</a>
 									<div class="mini-news">- Tham quan bán đảo Sơn Trà, ngắm cảng Tiên Sa, viếng chùa Linh Ứng Bãi Bụt - ngôi chùa lớn nhất ở thành phố Đà Nẵng, chiêm bái tượng Phật Quan Thế Âm cao nhất Việt Nam.
@@ -108,7 +100,82 @@
 							<div class="tang1">
 								<div class="col-md-12">
 									<div class="row">
-										<h1>Catalog - Chọn tour :</h1>
+										<h1>Tour yêu thích :</h1>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="row">
+										<div class="ctg-sanpham">
+											<a href="test.html">
+												<img src="image/halong.jpg">
+											</a>
+											<div>Ha Long</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="row">
+										<div class="ctg-sanpham">
+											<a href="test.html">
+												<img src="image/hoankiem.jpg">
+											</a>
+											<div>Hoan Kiem</div>
+										</div>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="row">
+										<div class="ctg-sanpham">
+											<a href="test.html">
+												<img src="image/hoian.jpg">
+											</a>
+											<div>Hoi An</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="tang2">
+							<div class="col-md-4">
+								<div class="row">
+									<div class="ctg-sanpham">
+										<a href="test.html">
+											<img src="image/halong2.jpeg">
+										</a>
+										<div>Ha Long</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="row">
+									<div class="ctg-sanpham">
+										<a href="test.html">
+											<img src="image/new1/3.jpg">
+										</a>
+										<div>Hoan Kiem</div>
+									</div>
+								</div>
+							</div>
+							<div class="col-md-4">
+								<div class="row">
+									<div class="ctg-sanpham">
+										<a href="test.html">
+											<img src="image/hoian.jpg">
+										</a>
+										<div>Hoi An</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="container">	
+					<div class="row">
+						<div class="catalog-bottom">
+							<div class="tang1">
+								<div class="col-md-12">
+									<div class="row">
+										<h1>Tour Trong Nước:</h1>
 									</div>
 								</div>
 								<div class="col-md-4">
@@ -183,5 +250,6 @@
 	<?php
 	include '4_bottom.php';
 	?>
+	<script type="text/javascript" src="js/myscript.js"></script>
 </body>
-</html>
+< /html>
